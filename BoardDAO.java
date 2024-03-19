b9f1352bf6d46cee07a272fba06dbb2840e9f2d2
xo-hyun <xo_hyunn@naver.com>
2024-03-19 11:26:05 +0900

    BoardDAO 수정 -insert,list[박 소 현]

--- BoardDAO.java
+++ BoardDAO.java
@@ -1,5 +1,13 @@
 class BoardDAO{
 	public void insert(){
+		System.out.println("BoardDAO!!!!!!!!!!!!!!");
 		System.out.println("게시판을 등록하였습니다.");
+	}
+	public List list(){
+		ArrayList = new ArrayList();
+		list.add("1번 게시물");
+		list.add("2번 게시물");
+		list.add("3번 게시물");
+		return list;
 	}
 }
